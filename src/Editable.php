cc1b45dde8c3584e8944ebd585471f5c329edd50
Mahmoud Gamal <Mahmoud Gamal>
2016-03-02 14:44:22 +0800

    add support to allow editable form owner

--- src/Editable.php
+++ src/Editable.php
@@ -13,21 +13,28 @@ class Editable extends Model
         $this->user = \Auth::user();
     }
 
-    public function text ($object, $column, $permissions = null)
+    public function text($object, $column, $permissions = null, $owner_id = 0, $editable_by_owner = false, $html = '//element//')
     {
-        if (isset($permissions) && $this->user->can($permissions)) {
-            return '<span v-if="!appLoaded">'.$object->$column.'</span><editable-text model="'.get_class($object).'" row-id="'.$object->id.'" key="'.$column.'" value="'.$object->$column.'"></editable-text>';
-        }
-        
-        return '<span>'.$object->$column.'</span>';
+        return $this->create_element('text', $object, $column, $permissions, $owner_id, $editable_by_owner, $html);
     }
 
-    public function textarea ($object, $column, $permissions = null)
+    public function textarea($object, $column, $permissions = null, $owner_id = 0, $editable_by_owner = false, $html = '//element//')
     {
-        if (isset($permissions) && $this->user->can($permissions)) {
-            return '<span v-if="!appLoaded">'.$object->$column.'</span><editable-textarea model="'.get_class($object).'" row-id="'.$object->id.'" key="'.$column.'" value="'.$object->$column.'"></editable-textarea>';
+        return $this->create_element('textarea', $object, $column, $permissions, $owner_id, $editable_by_owner, $html);
+    }
+
+    private function create_element($type, $object, $column, $permissions, $owner_id, $editable_by_owner, $html)
+    {
+        if ((isset($permissions) && $this->user->can($permissions)) ||
+            ($owner_id != 0 && $owner_id == $this->user->id && $editable_by_owner)) {
+            $element = '<span v-if="!appLoaded">'.$object->$column.'</span>';
+            $element += '<editable-'.$type.' model="'.get_class($object).'" row-id="'.$object->id.'" key="'.$column.'" value="'.$object->$column.'"></editable-'.$type.'>';
+        } elseif (!empty($object->$column)) {
+            $element = '<span>'.$object->$column.'</span>';
+        } else {
+            return '';
         }
-        
-        return '<span>'.$object->$column.'</span>';
+
+        return str_replace('//element//', $element, $html);
     }
 }
